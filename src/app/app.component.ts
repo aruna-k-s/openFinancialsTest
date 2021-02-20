b88e36213080445f681bdd37b1a322b27fcb3e56
aruna-k-s <call2arunks@gmail.com>
2021-02-20 19:21:28 +0530

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'openFinancialsTest';
+  applyClassToNavBar: boolean;
+  monthlyIncome = 0;
+  monthlyExpense = 0;
+  rePayment= new FormControl('0');
+
+  ngOnInit() {
+    this.rePayment = new FormControl('6')
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 25) {
+      this.applyClassToNavBar = true;
+    } else {
+      this.applyClassToNavBar = false;
+    }
+
+  }
+
+  updateValue(e) {
+      this.monthlyIncome = e.value  ;
+    
+  }
+  updateValueMonthlyExp(event) {
+    this.monthlyExpense = event.value ;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
